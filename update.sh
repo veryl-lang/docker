a6162a31568a714f009c5d78cb8f4a094a59d982
dalance <dalance@gmail.com>
2025-10-01 14:05:54 +0900

    Add release script

--- update.sh
+++ update.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-find . -name Dockerfile -print0 | xargs -0 sed -i -e "s/VERYL_VERSION=.*/VERYL_VERSION=$1/g"
+
+# $1 is git tag name (e.g. v0.16.0)
+version=${1:1}
+
+find . -name Dockerfile -print0 | xargs -0 sed -i -e "s/VERYL_VERSION=.*/VERYL_VERSION=$version/g"
